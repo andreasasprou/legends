59d8ad1500636fc432bac4f1d7918562a68ef2da
Andreas Asprou <andyasprou@gmail.com>
2025-07-09 19:13:52 +0300

    fix: build

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ export default defineConfig({
   root: './',
   server: {
     port: 3000,
-    open: '/legendary-football-landing.html'
+    open: '/index.html'
   },
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main: './legendary-football-landing.html'
+        main: './index.html'
       }
     }
   }
